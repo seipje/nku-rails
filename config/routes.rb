88b30b4cd975646977e6f6b0ab44b4dd7f447fc0
Jessica <seipje@gmail.com>
2014-01-20 18:05:02 +0000

    Told Rails where we want to navigate to see comments

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ NkuRails::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
  
 
   # Example of regular route:
